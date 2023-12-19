ffcb421cddc4700e93159c671651743271dfcf6e
GuiLeoni14 <guileone167@gmail.com>
2023-12-18 22:09:43 -0300

    chore

--- src/app/servicos/criacao-de-landing-page/page.tsx
+++ src/app/servicos/criacao-de-landing-page/page.tsx
@@ -1,7 +1,14 @@
 import { Button } from '@/components/Button'
+import { CallToAction } from '@/components/CallToAction'
 import { CardHover } from '@/components/CardHover'
 import { TechnologySlide } from '@/components/TechnologySlide'
-import { Chrome, LayoutTemplate } from 'lucide-react'
+import {
+  Chrome,
+  LayoutTemplate,
+  PackageOpen,
+  PaintBucket,
+  TrendingUp,
+} from 'lucide-react'
 
 export default async function LpServicePage() {
   return (
@@ -44,17 +51,60 @@ export default async function LpServicePage() {
       </section>
       <section className="bg-gray-900 py-20">
         <div className="container mx-auto">
-          <div className="flex justify-between">
-            <h2 className="mt-2 text-2xl font-bold md:text-4xl">
-              Entenda o que engloba o processo de Desenvolvimento de Site:
-            </h2>
-            <span className="block max-w-[420px] text-lg md:text-xl">
-              Desenvolvimento de sites exclusivos, adaptados para uma gama de
-              dispositivos e planejados para sua empresa conquistar novos
-              clientes.
-            </span>
+          <span className="block bg-gradient-title bg-clip-text text-center text-4xl font-bold text-transparent lg:text-6xl">
+            Entenda o meu processo
+          </span>
+          <h2 className="mt-2 text-center text-2xl font-bold md:text-4xl">
+            Entenda o que engloba o processo de criação de uma landing page
+          </h2>
+          <div className="mt-16 grid grid-cols-3 gap-8">
+            <div className="flex h-full flex-col items-start justify-center rounded-md bg-gray-700 p-8">
+              <div className="flex h-[60px] w-[60px] items-center justify-center rounded-md border border-gray-200 bg-gray-700 shadow-2xl">
+                <TrendingUp />
+              </div>
+              <strong className="mt-8 text-4xl font-bold">
+                <span className="bg-gradient-title bg-clip-text text-transparent">
+                  01.
+                </span>{' '}
+                Start
+              </strong>
+              <p className="mt-4 text-base">
+                Entendemos as suas demandas e o fluxo de telas, para montar um
+                projeto personalizado.
+              </p>
+            </div>
+            <div className="flex h-full flex-col items-start justify-center bg-gray-700 p-8">
+              <div className="flex h-[60px] w-[60px] items-center justify-center rounded-md border border-gray-200 bg-gray-700 shadow-2xl">
+                <PaintBucket />
+              </div>
+              <strong className="mt-8 text-4xl font-bold">
+                <span className="bg-gradient-title bg-clip-text text-transparent">
+                  02.
+                </span>{' '}
+                Design
+              </strong>
+              <p className="mt-4 text-base">
+                Desenvolvemos o layout de telas focadas na usabilidade e em
+                atender às suas demandas.
+              </p>
+            </div>
+            <div className="flex h-full flex-col items-start justify-center bg-gray-700 p-8">
+              <div className="flex h-[60px] w-[60px] items-center justify-center rounded-md border border-gray-200 bg-gray-700 shadow-2xl">
+                <PackageOpen />
+              </div>
+              <strong className="mt-8 text-4xl font-bold">
+                <span className="bg-gradient-title bg-clip-text text-transparent">
+                  03.
+                </span>{' '}
+                Entrega
+              </strong>
+              <p className="mt-4 text-base">
+                Evoluímos e estamos disponíveis para sempre atualizar seu
+                projeto.
+              </p>
+            </div>
           </div>
-          <div className="flex justify-between gap-8 pt-20">
+          <div className="flex gap-8 pt-40">
             <CardHover className="w-full max-w-[348px]">
               <div
                 className="flex h-[644px] w-full max-w-[348px] flex-col items-start gap-2 rounded-md border border-transparent
@@ -72,6 +122,34 @@ export default async function LpServicePage() {
                 </div>
               </div>
             </CardHover>
+            <div className="flex flex-1 flex-col">
+              <h2 className="text-2xl font-bold md:text-4xl">
+                Seu layout se encaixando em qualquer dispositivo
+              </h2>
+              <p className="mt-4 text-base">
+                Site responsivo é aquele que se adapta a qualquer tamanho de
+                tela. Essa adaptação não é apenas uma questão técnica, já que
+                influencia também a experiência do usuário no site e a
+                satisfação dos consumidores com a marca.{' '}
+              </p>
+              <img
+                className="mt-auto"
+                src="https://zyro.com/cdn-cgi/image/w=1100,q=95,f=auto/images/landingWebsite/hero.png"
+              />
+            </div>
+          </div>
+          <div className="flex gap-8 pt-40">
+            <div className="flex-1">
+              <h2 className="text-2xl font-bold md:text-4xl">
+                Seu layout se encaixando em qualquer navegador
+              </h2>
+              <p className="mt-4 text-base">
+                Site responsivo é aquele que se adapta a qualquer tamanho de
+                tela. Essa adaptação não é apenas uma questão técnica, já que
+                influencia também a experiência do usuário no site e a
+                satisfação dos consumidores com a marca.{' '}
+              </p>
+            </div>
             <CardHover className="w-full max-w-[836px]">
               <div
                 className="flex h-[645px] w-full max-w-full flex-col items-start gap-2 rounded-md border border-transparent
@@ -91,6 +169,9 @@ export default async function LpServicePage() {
             </CardHover>
           </div>
         </div>
+      </section>
+      <section className="container mx-auto max-w-5xl py-20 md:py-40">
+        <CallToAction />
       </section>
     </main>
   )
